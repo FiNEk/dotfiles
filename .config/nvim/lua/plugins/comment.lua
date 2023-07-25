6237652fb3955422d46186214b66da333dc5ba5f
Denis Anashkin <fnk007@yandex.ru>
2023-07-26 01:48:16 +0300

    feat(nvim): fix node debugger, introduce comment toggle

---
+++ .config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      toggler = {
+        line = "<leader>/",
+        block = nil,
+      },
+      opleader = {
+        line = "<leader>/",
+        block = nil,
+      },
+      mappings = {
+        basic = true,
+        extra = false,
+      },
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    opts = function()
+      local dap = require("dap")
+      require("dap").adapters["pwa-node"] = {
+        type = "server",
+        host = "127.0.0.1",
+        port = "${port}",
+        executable = {
+          command = "node",
+          args = {
+            require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+              .. "/js-debug/src/dapDebugServer.js",
+            "${port}",
+          },
+        },
+      }
+      for _, language in ipairs({ "typescript", "javascript" }) do
+        if not dap.configurations[language] then
+          dap.configurations[language] = {
+            {
+              type = "pwa-node",
+              request = "launch",
+              name = "Launch file",
+              program = "${file}",
+              cwd = "${workspaceFolder}",
+            },
+            {
+              type = "pwa-node",
+              request = "attach",
+              name = "Attach",
+              processId = require("dap.utils").pick_process,
+              cwd = "${workspaceFolder}",
+            },
+          }
+        end
+      end
+    end,
+  },
+}
