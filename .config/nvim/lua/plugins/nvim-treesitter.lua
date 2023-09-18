3c15d61d19b5d0766d742554b6db1661a1e5ce2f
Denis Anashkin <fnk007@yandex.ru>
2023-09-18 20:25:26 +0300

    nvim: upgrade typescript tools, bring back tab binds. change theme

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,16 +1,15 @@
-local utils = require("utils")
-local required_parsers = { "typescript", "tsx", "vue" }
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = { "andymass/vim-matchup" },
-    init = function()
-      vim.g.matchup_matchparen_offscreen = { method = "popup", fullwidth = 1, highlight = "Normal", syntax_hl = 1 }
-      vim.g.matchup_matchparen_deferred = 1
-    end,
+    -- dependencies = { "andymass/vim-matchup" },
+    -- init = function()
+    --   vim.g.matchup_matchparen_offscreen = { method = "popup", fullwidth = 1, highlight = "Normal", syntax_hl = 1 }
+    --   vim.g.matchup_matchparen_deferred = 1
+    -- end,
     opts = function(_, opts)
+      local utils = require("utils")
+      local required_parsers = { "typescript", "tsx", "vue" }
       utils.list.extend_or_create(opts.ensure_installed, required_parsers)
-      opts.matchup = { enable = true }
     end,
   },
 }
--- .config/nvim/lua/plugins/typescript-tools.lua
+++ .config/nvim/lua/plugins/typescript-tools.lua
@@ -3,10 +3,10 @@ return {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = function(_, opts)
-      local mason_registry = require("mason-registry")
-      local tsserver_path = mason_registry.get_package("typescript-language-server"):get_install_path()
+      -- local mason_registry = require("mason-registry")
+      -- local tsserver_path = mason_registry.get_package("typescript-language-server"):get_install_path()
       opts.settings = {
-        tsserver_path = tsserver_path .. "/node_modules/typescript/lib/tsserver.js",
+        -- tsserver_path = tsserver_path .. "/node_modules/typescript/lib/tsserver.js",
         expose_as_code_action = {
           "fix_all",
           "add_missing_imports",
