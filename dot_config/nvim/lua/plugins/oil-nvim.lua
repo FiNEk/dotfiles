538db7be5d2c3b1746fdcd85ae5d10147cb67699
FiNEk <fnk007@yandex.ru>
2024-02-20 17:33:38 +0300

    remap oil.nvim keys

--- dot_config/nvim/lua/plugins/oil-nvim.lua
+++ dot_config/nvim/lua/plugins/oil-nvim.lua
@@ -1,7 +1,11 @@
 return {
   "stevearc/oil.nvim",
   lazy = false,
-  opts = {},
+  opts = {
+    keymaps = {
+      ["<q>"] = "actions.close",
+    },
+  },
   keys = {
     { "<leader>e", "<CMD>Oil<CR>", desc = "Open parent directory" },
   },
